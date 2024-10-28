c59e271fe4a18d82c13b25cfec254758052d14c0
Franco Valledor <francovalledor@gmail.com>
2024-10-27 21:38:05 -0300

    configure react-query

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { COIN_API_KEY } from "./config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Coins } from "./Coins";
 
 export default function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { retry: 2 } },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>key is {COIN_API_KEY}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </QueryClientProvider>
   );
 }
 
--- client/useExchanges.ts
+++ client/useExchanges.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { COIN_API_KEY } from "../config";
+import { COIN_API_KEY } from "../config/config";
+import { useQuery } from "@tanstack/react-query";
+import { keyBy, noop } from "lodash";
 
 interface Exchange {
   exchange_id: string;
@@ -24,5 +26,33 @@ const coinApi = axios.create({
   },
 });
 
-export const getExchanges = async () =>
+const getExchanges = async () =>
   coinApi.get<Exchange[]>("/v1/exchanges").then((res) => res.data);
+
+type IconResp = {
+  exchange_id: string;
+  url: string;
+};
+
+const getIcons = async () =>
+  coinApi.get<IconResp[]>("/v1/exchanges/icons/256").then((res) => res.data);
+
+const getExchangesWithIcons = async () => {
+  const [exchanges, icons] = await Promise.all([getExchanges(), getIcons()]);
+
+  const iconsByExchange = keyBy(icons, (ic) => ic.exchange_id);
+
+  return exchanges.map((exc) => ({
+    ...exc,
+    icon: iconsByExchange[exc.exchange_id],
+  }));
+};
+
+const A_WHOLE_DAY = 60 * 60 * 24 * 1000;
+
+export const useCoins = () =>
+  useQuery({
+    queryKey: ["coinList"],
+    queryFn: getExchangesWithIcons,
+    staleTime: A_WHOLE_DAY,
+  });
