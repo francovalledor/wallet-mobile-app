ff6b117bf16f8e4a873758ec1e0044a636cd0baf
Franco Valledor <francovalledor@gmail.com>
2024-11-03 14:44:22 -0300

    remove old project components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Exchanges } from "./components/Exchanges";
 
 export default function App() {
   const queryClient = new QueryClient({
